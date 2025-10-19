a79f1e157a9e2587b06db7d37ed2120c9ede3736
Julia Gabriela <juliagabrielagomes09@gmail.com>
2025-10-19 10:51:37 -0300

    Fixsi

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, "home.html")
+def home_view(request):
+    return render(request, 'home.html')
 
-def sobre_nos(request):
-    return render(request, "sobre.html")
+def login_view(request):
+    return render(request, 'login.html')
+
+def sobre_view(request):
+    return render(request, 'sobre.html')
