c6b529fc663261d7bb4487c39ac5a877bf45be79
Maria Eduarda Nunes Bezerra <depositodanunes@gmail.com>
2025-11-06 01:53:25 -0300

    detalhes

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,13 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),        
-    
-    # 2. Inclui as URLs do app 'login' (ex: /login/)
     path('login/', include('login.urls')),  
     path('', include('app.urls')), 
-
-    # 4. As linhas abaixo foram REMOVIDAS daqui,
-    # pois elas agora vivem dentro de 'app.urls.py'
 ]
 
 if settings.DEBUG:
